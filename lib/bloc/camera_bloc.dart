90d65cc5b43193788571ef6396b13524ee203f80
nauvalachid <nauvalachid@gmail.com>
2025-05-16 02:38:05 +0700

    Menambahkan fungsi untuk mengambil gambar di CameraBloc

--- lib/bloc/camera_bloc.dart
+++ lib/bloc/camera_bloc.dart
@@ -51,4 +51,12 @@ class CameraBloc extends Bloc<CameraEvent, CameraState> {
     await s.controller.setFlashMode(next);
     emit(s.copyWith(flashMode: next));
   }
+
+  Future<void> _onTakePicture(
+    TakePicture event,Emitter<CameraState> emit) async {
+    if (state is! CameraReady) return;
+    final s = state as CameraReady;
+    final file = await s.controller.takePicture();
+    event.onPictureTaken(File(file.path));
+  }
 }
