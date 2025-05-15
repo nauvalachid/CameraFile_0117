096538c596e5a1b6b0735b9851ecdeb08ed9fd0d
nauvalachid <nauvalachid@gmail.com>
2025-05-16 03:10:03 +0700

    Refaktor impor di camera_bloc.dart dan camera_state.dart untuk konsistensi

--- lib/bloc/camera_bloc.dart
+++ lib/bloc/camera_bloc.dart
@@ -6,8 +6,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-part 'camera_event.dart';
-part 'camera_state.dart';
+import 'camera_event.dart';
+import 'camera_state.dart';
 
 class CameraBloc extends Bloc<CameraEvent, CameraState> {
   late final List<CameraDescription> _cameras;
