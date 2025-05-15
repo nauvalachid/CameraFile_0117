934a8de64184b23e40e141174ff79e0a0d3ecb9c
nauvalachid <nauvalachid@gmail.com>
2025-05-16 02:34:23 +0700

    Terapkan fungsi peralihan kamera di CameraBloc

--- lib/bloc/camera_bloc.dart
+++ lib/bloc/camera_bloc.dart
@@ -31,4 +31,11 @@ class CameraBloc extends Bloc<CameraEvent, CameraState> {
     
     await _setupController(0, emit);
   }
+
+  Future<void> _onSwitch(SwitchCamera event, Emitter<CameraState> emit) async {
+    if (state is! CameraReady) return;
+    final s = state as CameraReady;
+    final next = (s.selectedIndex + 1) % _cameras.length;
+    await _setupController(next, emit, previous: s);
+  }
 }
