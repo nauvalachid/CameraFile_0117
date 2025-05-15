9d26c6da57de5d2a3dff32810d29c89810c7ad11
nauvalachid <nauvalachid@gmail.com>
2025-05-16 03:12:48 +0700

    Menambahkan kelas RequestPermissions pada CameraEvent untuk mendukung permintaan izin

--- lib/bloc/camera_event.dart
+++ lib/bloc/camera_event.dart
@@ -30,3 +30,5 @@ final class OpenCameraAndCapture extends CameraEvent {
 final class DeleteImage extends CameraEvent {}
 
 final class ClearSnackbar extends CameraEvent {}
+
+final class RequestPermissions extends CameraEvent {}
