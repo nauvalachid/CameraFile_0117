62982423000f928ff66839c4d16e95400a618945
nauvalachid <nauvalachid@gmail.com>
2025-05-16 02:28:27 +0700

    Refaktor camera_bloc.dart untuk menyertakan impor yang diperlukan untuk fungsionalitas kamera

--- lib/bloc/camera_bloc.dart
+++ lib/bloc/camera_bloc.dart
@@ -1,5 +1,10 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 part 'camera_event.dart';
 part 'camera_state.dart';
