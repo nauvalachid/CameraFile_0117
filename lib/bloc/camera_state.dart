5421353af6efb82ef65895a37469e771ccfe3a7d
nauvalachid <nauvalachid@gmail.com>
2025-05-16 03:49:51 +0700

    Menambahkan kelas CameraReady untuk mengelola status kamera dengan controller, indeks terpilih, mode flash, file gambar, dan pesan snackbar

--- lib/bloc/camera_state.dart
+++ lib/bloc/camera_state.dart
@@ -4,3 +4,11 @@ import 'package:camera/camera.dart';
 sealed class CameraState {}
 
 final class CameraInitial extends CameraState {}
+
+final class CameraReady extends CameraState {
+  final CameraController controller;
+  final int selectedIndex;
+  final FlashMode flashMode;
+  final File? imageFile;
+  final String? snackbarMessage;
+}
