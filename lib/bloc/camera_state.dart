8ca2486f72f02726d9c8fcf4e774e343e11a3dcb
nauvalachid <nauvalachid@gmail.com>
2025-05-16 03:50:25 +0700

    Menambahkan konstruktor untuk kelas CameraReady dengan parameter yang diperlukan

--- lib/bloc/camera_state.dart
+++ lib/bloc/camera_state.dart
@@ -11,4 +11,12 @@ final class CameraReady extends CameraState {
   final FlashMode flashMode;
   final File? imageFile;
   final String? snackbarMessage;
+
+  CameraReady({
+    required this.controller,
+    required this.selectedIndex,
+    required this.flashMode,
+    this.imageFile,
+    this.snackbarMessage,
+  });
 }
