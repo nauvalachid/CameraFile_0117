68eb0b92ce3dedf5968af7439b82afc6dbc010f5
nauvalachid <nauvalachid@gmail.com>
2025-05-16 04:29:43 +0700

    Memperbaiki path direktori untuk penyimpanan gambar di StorageHelper

--- lib/storage_helper.dart
+++ lib/storage_helper.dart
@@ -3,8 +3,7 @@ import 'package:path/path.dart' as path;
 
 class StorageHelper {
   static Future<String> _getFolderPath() async {
-    final Directory dir =
-        Directory('/storage/emulated/0/DCIM/FlutterNativeCam');
+    final dir = Directory('/storage/emulated/0/DCIM/FlutterNativeCamBloc');
     if (!await dir.exists()) await dir.create(recursive: true);
     return dir.path;
   }
