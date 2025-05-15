87963206a2361d2bb6b57a279efec0124f9816a6
nauvalachid <nauvalachid@gmail.com>
2025-05-16 02:13:41 +0700

    Perbaiki versi NDK di build.gradle.kts dan perbaiki jalur direktori di storage_helper.dart

--- lib/storage_helper.dart
+++ lib/storage_helper.dart
@@ -4,16 +4,16 @@ import 'package:path/path.dart' as path;
 class StorageHelper {
   static Future<String> _getFolderPath() async {
     final Directory dir =
-        Directory('/strorage/emulated/0/DCIM/FlutterNativeCam');
+        Directory('/storage/emulated/0/DCIM/FlutterNativeCam');
     if (!await dir.exists()) await dir.create(recursive: true);
     return dir.path;
   }
 
   static Future<File> saveImage(File file, String prefix) async {
     final String dirPath = await _getFolderPath();
-    final String fileName = 
-       '${prefix}_${DateTime.now().millisecondsSinceEpoch}${path.extension(file.path)}';
-    final String savedPath = path.join(dirPath, fileName);
+    final String filename = 
+       '${prefix}${DateTime.now().millisecondsSinceEpoch}${path.extension(file.path)}';
+    final String savedPath = path.join(dirPath, filename);
     return await file.copy(savedPath);
   }
 }
