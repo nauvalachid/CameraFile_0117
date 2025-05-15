f167c9a6962746c862f93294e3452e3128f2a995
nauvalachid <nauvalachid@gmail.com>
2025-05-16 03:08:09 +0700

    Menambahkan kelas TakePicture pada CameraEvent untuk menangani pengambilan gambar

--- lib/bloc/camera_event.dart
+++ lib/bloc/camera_event.dart
@@ -8,3 +8,8 @@ final class InitializeCamera extends CameraEvent {}
 final class SwitchCamera extends CameraEvent {}
 
 final class ToggleFlash extends CameraEvent {}
+
+final class TakePicture extends CameraEvent {
+  final void Function(File imageFile) onPictureTaken;
+  TakePicture(this.onPictureTaken);
+}
