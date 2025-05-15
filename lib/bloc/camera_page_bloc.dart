9a1e7e377bf7e8caf6ee8696ec38e72499234e50
nauvalachid <nauvalachid@gmail.com>
2025-05-16 03:33:02 +0700

    Menambahkan file camera_page_bloc.dart dengan import yang diperlukan untuk pengelolaan kamera

---
+++ lib/bloc/camera_page_bloc.dart
@@ -0,0 +1,6 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'camera_bloc.dart';
+import 'camera_event.dart';
+import 'camera_state.dart';
