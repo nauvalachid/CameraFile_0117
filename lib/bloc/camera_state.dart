ea0772dd54ac1e20203bd59f729e35f5ec0d9a79
nauvalachid <nauvalachid@gmail.com>
2025-05-16 03:51:05 +0700

    Menambahkan metode copyWith pada kelas CameraReady untuk mendukung penggandaan objek dengan modifikasi properti

--- lib/bloc/camera_state.dart
+++ lib/bloc/camera_state.dart
@@ -19,4 +19,16 @@ final class CameraReady extends CameraState {
     this.imageFile,
     this.snackbarMessage,
   });
+
+  CameraReady copyWith({
+    CameraController? controller,
+    int? selectedIndex,
+    FlashMode? flashMode,
+    File? imageFile,
+    String? snackbarMessage,
+    bool clearSnackbar = false,
+  }) {
+  
+  
+  }
 }
