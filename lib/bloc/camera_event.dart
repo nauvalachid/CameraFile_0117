4466ee5a2ace8156ba4c7bfd106896d583c67b3c
nauvalachid <nauvalachid@gmail.com>
2025-05-16 03:08:54 +0700

    Menambahkan kelas TapToFocus pada CameraEvent untuk mendukung fokus pada posisi tertentu

--- lib/bloc/camera_event.dart
+++ lib/bloc/camera_event.dart
@@ -13,3 +13,9 @@ final class TakePicture extends CameraEvent {
   final void Function(File imageFile) onPictureTaken;
   TakePicture(this.onPictureTaken);
 }
+
+final class TapToFocus extends CameraEvent {
+  final Offset position;
+  final Size previewSize;
+  TapToFocus(this.position, this.previewSize);
+}
